2cd77cbb41e69e61cd52c2d85ebd6dfd1417b23d
Joaquin <jtalon@stevens.edu>
2019-06-22 23:50:20 -0400

    added dateparser for datetime

---
+++ dateparser.py
@@ -0,0 +1,27 @@
+#2017-10-03 22:20:37 EDT
+
+class DateParser:
+    def __init__(self):
+        pass
+    def getDate(datestring):
+        raw_date = datestring[0:10]
+        date_array = raw_date.split("-")
+        
+        return_date = []
+        for s in date_array:
+            return_date.append(int(s))
+        return return_date
+    def getTime(datestring):
+        raw_time = datestring[11:19]
+        time_array = raw_time.split(":")
+
+        return_time = []
+        for s in time_array:
+            return_time.append(int(s))
+        return return_time
+    def getDateTime(datestring):
+        return DateParser.getDate(datestring) + DateParser.getTime(datestring)
+
+print(DateParser.getDate("2017-10-03 22:20:37 EDT"))
+print(DateParser.getTime("2017-10-03 22:20:37 EDT"))
+print(DateParser.getDateTime("2017-10-03 22:20:37 EDT"))
